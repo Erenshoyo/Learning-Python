2d11a15a66c4b9344ecf4d337a183bb27d76d745
Md. Asif Shahriar Tauhid <70537695+Erenshoyo@users.noreply.github.com>
2025-06-02 12:32:22 +0600

    To do list 2

--- To_Do_List/main.py
+++ To_Do_List/main.py
@@ -1,1 +1,13 @@
-print("Enter a ToDo: ")
+# Type-1:
+# print("Enter a ToDo: ")
+# user_text = input()
+# print(user_text)
+
+# Type-2:
+# user_text = input("Enter a Todo: ")
+# print(user_text)
+
+# It is also possible to store a sentence in a variable.
+prompt = "Enter a ToDo: "
+user_text = input(prompt)
+print(user_text)
