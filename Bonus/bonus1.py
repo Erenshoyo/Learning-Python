ad3ad3bc8838dd8dc59f34c6bb14980bb1d40430
Md. Asif Shahriar Tauhid <70537695+Erenshoyo@users.noreply.github.com>
2025-06-03 12:33:06 +0600

    bonus

---
+++ Bonus/bonus1.py
@@ -0,0 +1,5 @@
+text = input("Enter a title: ")
+
+# len() is a function that prints the character count in a string.
+length = len(text)
+print("The length of this sentence is ", length, " characters")
