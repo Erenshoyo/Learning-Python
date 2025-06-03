6f58864e038d141c9fb4e85e9dd7ccfb72bc07a6
Md. Asif Shahriar Tauhid <70537695+Erenshoyo@users.noreply.github.com>
2025-06-03 12:17:06 +0600

    multiple user input-testing

--- To_Do_List/main.py
+++ To_Do_List/main.py
@@ -7,7 +7,15 @@
 # user_text = input("Enter a Todo: ")
 # print(user_text)
 
-# It is also possible to store a sentence in a variable.
+# It is also possible to store a sentence in a variable in python.
 prompt = "Enter a ToDo: "
-user_text = input(prompt)
-print(user_text)
+user_text1 = input(prompt)
+user_text2 = input(prompt)
+user_text3 = input(prompt)
+
+user_texts = [user_text1, user_text2, user_text3, "Hello"]  # python list
+
+print(user_texts)
+
+print(type(prompt))
+print(type(user_texts))
