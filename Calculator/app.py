5fe96813e7658f2b6624bb813efc619b45f5ad58
Md. Asif Shahriar Tauhid <70537695+Erenshoyo@users.noreply.github.com>
2025-07-09 21:18:11 +0600

    Calculator Project

---
+++ Calculator/app.py
@@ -0,0 +1,23 @@
+# Python calculator
+
+num1 = float(input("Enter the first number: "))
+
+num2 = float(input("Enter the second number: "))
+
+operator = input("Enter an operation ( + , - , * , / ):")
+
+if operator == "+":
+    result = num1 + num2
+    print(result)
+
+elif operator == "-":
+    result = num1 - num2
+    print(result)
+
+elif operator == "*":
+    result = num1 * num2
+    print(result)
+
+elif operator == "/":
+    result = num1 / num2
+    print(result)
